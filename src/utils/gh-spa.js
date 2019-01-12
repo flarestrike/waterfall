dc19eaf0a6941043de34463f5a108e27dcdb1d60
bmVsc29u <bmVsc29u@gmail.com>
2019-01-12 18:22:26 +0800

    fix: gh-spa import error

--- src/utils/gh-spa.js
+++ src/utils/gh-spa.js
@@ -1,9 +1,7 @@
-export function spa() {
+(function() {
   const { redirect } = sessionStorage;
   delete sessionStorage.redirect;
   if (redirect && redirect !== location.href) {
     history.replaceState(null, null, redirect);
   }
-}
-
-spa();
+})();
