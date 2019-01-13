1d85359a34c0cc6c768ebfe16313b670e245b2c0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-13 09:27:44 +0000

    Deploy flarestrike/waterfall to github.com/flarestrike/waterfall.git:gh-pages

---
+++ assets/fonts/common.ts
@@ -0,0 +1,22 @@
+export const common = {
+  name: 'common',
+  list: {
+    mail: '\e900',
+    hat: '\e901',
+    tree: '\e902',
+    male: '\e903',
+    female: '\e904',
+    world: '\e905',
+    trophy: '\e906',
+    award: '\e907',
+    folder_star: '\e908',
+    folder_shared: '\e909',
+    building: '\e90a',
+    clock: '\e90b',
+    image: '\e90c',
+    pin: '\e90d',
+    suitcase: '\e90e',
+    user: '\e90f',
+    folder_images: '\e910',
+  }
+};
