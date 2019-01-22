4af8e379cf6245e2eb07232533813d45fea16b1c
bmVsc29u <bmVsc29u@gmail.com>
2019-01-23 01:00:32 +0800

    chore: fix linter

--- src/utils/locales.ts
+++ src/utils/locales.ts
@@ -1,6 +1,6 @@
 declare var require: any;
 const keys = ['en', 'hant', 'hans'];
-export const locales = keys.reduce((r, k) =>{
+export const locales = keys.reduce((r, k) => {
   r[k] = require(`../assets/locale/${k}.json`);
   return r;
 }, {});
