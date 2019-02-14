20219e9dffab6f584f29a8a8f212ff4dd6345b57
bmVsc29u <bmVsc29u@gmail.com>
2019-02-15 00:39:03 +0800

    feat: polish nav

--- src/layout/app.layout.ts
+++ src/layout/app.layout.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { HostBinding, Inject, HostListener, Component } from '@angular/core';
 
 @Component({
   selector: 'wl-app-layout',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['app.layout.sass']
 })
 export class WlAppLayout {
-
+  tophMax: number;
+  toph: number;
+  gtop = false;
+  wscroll(e) {
+    const t = e.target.scrollTop;
+    if (!this.tophMax) {
+      this.tophMax = e.target.offsetTop;
+    }
+    this.toph = Math.max(this.tophMax - t, 40);
+    this.gtop = this.toph < 41;
+  }
 }
