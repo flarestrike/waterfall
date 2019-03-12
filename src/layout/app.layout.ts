bf3e497e8a274f1e4d5fa6825dab9347062da2f9
bmVsc29u <bmVsc29u@gmail.com>
2019-03-12 12:21:33 +0800

    feat: rwd nav things

--- src/layout/app.layout.ts
+++ src/layout/app.layout.ts
@@ -2,7 +2,6 @@ import { ViewChild, ElementRef, AfterViewInit,
   HostBinding, Inject, HostListener, Component } from '@angular/core';
 import { platformWindow } from '@mod/utils';
 
-const dmin = 40;
 @Component({
   selector: 'wl-app-layout',
   templateUrl: 'app.layout.html',
@@ -15,7 +14,10 @@ export class WlAppLayout implements AfterViewInit {
   toph: number;
   gtop = false;
   constructor(@Inject(platformWindow) private wnd: Window) {}
-  @HostListener('window:scroll', ['$event']) scroll(e) {
+  @HostListener('window:resize', ['$event']) wresize(e) {
+    this.update(this.wnd.scrollY);
+  }
+  @HostListener('window:scroll', ['$event']) wscroll(e) {
     this.update(this.wnd.scrollY);
   }
   ngAfterViewInit() {
@@ -24,6 +26,7 @@ export class WlAppLayout implements AfterViewInit {
     });
   }
   private update(y) {
+    const dmin = this.wnd.innerWidth >= 600 ? 50 : 80;
     this.toph = Math.max(this.dmax - y, dmin);
     this.gtop = this.toph <= dmin;
     this.top.nativeElement.childNodes.forEach(c => {
